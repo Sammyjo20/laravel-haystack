9db788bd0e97821437808190928af38284805fd9
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2022-07-28 20:04:53 +0100

    wip

--- tests/Feature/HaystackTest.php
+++ tests/Feature/HaystackTest.php
@@ -1,15 +1,65 @@
 <?php
 
+use Illuminate\Support\Facades\Queue;
+use Sammyjo20\LaravelHaystack\Models\Haystack;
+use Sammyjo20\LaravelHaystack\Tests\Fixtures\Jobs\NameJob;
+
 test('you can start a haystack', function () {
-    //
+    Queue::fake();
+
+    $haystack = Haystack::build()
+        ->addJob(new NameJob('Sam'))
+        ->create();
+
+    Queue::assertNothingPushed();
+
+    $haystack->start();
+
+    expect($haystack->started)->toBeTrue();
+
+    Queue::assertPushed(NameJob::class);
 });
 
-test('you can finish a haystack', function () {
-    //
+test('you can finish a haystack early', function () {
+    Queue::fake();
+
+    $variable = 0;
+
+    $haystack = Haystack::build()
+        ->addJob(new NameJob('Sam'))
+        ->then(function () use (&$variable) {
+            $variable++;
+        })
+        ->create();
+
+    expect($variable)->toEqual(0);
+
+    $haystack->finish();
+
+    expect($variable)->toEqual(1);
+
+    Queue::assertNothingPushed();
 });
 
 test('you can fail a haystack', function () {
-    //
+    Queue::fake();
+
+    $variable = 0;
+
+    $haystack = Haystack::build()
+        ->addJob(new NameJob('Sam'))
+        ->catch(function () use (&$variable) {
+            $variable++;
+        })
+        ->create();
+
+    expect($variable)->toEqual(0);
+
+    $haystack->fail();
+
+    expect($variable)->toEqual(1);
+
+    Queue::assertNothingPushed();
 });
 
 test('you can append a job onto the haystack', function () {
