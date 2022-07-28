0fd48e8aed1d761ec40623d74011aa86930ae8a1
Sammyjo20 <Sammyjo20@users.noreply.github.com>
2022-07-28 21:09:47 +0000

    Having a pint 🍺

--- tests/Unit/HaystackBuilderTest.php
+++ tests/Unit/HaystackBuilderTest.php
@@ -3,11 +3,11 @@
 use Sammyjo20\LaravelHaystack\Models\Haystack;
 use Sammyjo20\LaravelHaystack\Builders\HaystackBuilder;
 use Sammyjo20\LaravelHaystack\Data\PendingHaystackBale;
-use Sammyjo20\LaravelHaystack\Tests\Fixtures\Callables\InvokableMiddleware;
 use Sammyjo20\LaravelHaystack\Tests\Fixtures\Jobs\NameJob;
 use Sammyjo20\LaravelHaystack\Tests\Fixtures\Callables\Middleware;
 use Sammyjo20\LaravelHaystack\Tests\Fixtures\Jobs\NotStackableJob;
 use Sammyjo20\LaravelHaystack\Tests\Fixtures\Callables\InvokableClass;
+use Sammyjo20\LaravelHaystack\Tests\Fixtures\Callables\InvokableMiddleware;
 
 test('you can add jobs to the haystack builder', function () {
     $builder = new HaystackBuilder;
