08c24818afc2b49a87140e25c9cf78aa051d4fb3
Kristijan Šimenić <ksimenic1@gmail.com>
2023-02-22 12:11:01 +0100

    Improve tests

--- tests/Unit/SerializeJobMysqlTest.php
+++ tests/Unit/SerializeJobMysqlTest.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use Illuminate\Support\Facades\DB;
 use Sammyjo20\LaravelHaystack\Models\Haystack;
 use Sammyjo20\LaravelHaystack\Models\HaystackBale;
 use Sammyjo20\LaravelHaystack\Tests\Fixtures\Jobs\NameJob;
@@ -11,7 +10,7 @@ beforeEach(function () {
     $this->setupForMySqlTest();
 });
 
-test('you can pass model and it will be serialized', function () {
+test('you can pass model and it will be serialized/deserialized correctly', function () {
     $haystack = Haystack::factory()->create();
 
     $bale = new HaystackBale;
@@ -20,14 +19,9 @@ test('you can pass model and it will be serialized', function () {
     $bale->haystack()->associate($haystack);
     $bale->save();
 
-    $serialized_string = (string) DB::table('haystack_bales')
-        ->select('job')
-        ->first()
-        ->job;
-
     // For mysql, the serialized string is saved as-is
     $this->assertEquals(
-        'O:53:"Sammyjo20\LaravelHaystack\Tests\Fixtures\Jobs\NameJob":1:{s:4:"name";s:4:"name";}',
-        $serialized_string
+        new NameJob('name'),
+        HaystackBale::first()->job
     );
 });
--- tests/Unit/SerializeJobPgsqlTest.php
+++ tests/Unit/SerializeJobPgsqlTest.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use Illuminate\Support\Facades\DB;
 use Sammyjo20\LaravelHaystack\Models\Haystack;
 use Sammyjo20\LaravelHaystack\Models\HaystackBale;
 use Sammyjo20\LaravelHaystack\Tests\Fixtures\Jobs\NameJob;
@@ -11,7 +10,7 @@ beforeEach(function () {
     $this->setupForPgSqlTest();
 });
 
-test('you can pass model and it will be serialized', function () {
+test('you can pass model and it will be serialized/deserialized correctly', function () {
     $haystack = Haystack::factory()->create();
 
     $bale = new HaystackBale;
@@ -20,14 +19,9 @@ test('you can pass model and it will be serialized', function () {
     $bale->haystack()->associate($haystack);
     $bale->save();
 
-    $serialized_string = (string) DB::table('haystack_bales')
-        ->select('job')
-        ->first()
-        ->job;
-
     // For pgsql, the serialized string is additionally base64 encoded
     $this->assertEquals(
-        'Tzo1MzoiU2FtbXlqbzIwXExhcmF2ZWxIYXlzdGFja1xUZXN0c1xGaXh0dXJlc1xKb2JzXE5hbWVKb2IiOjE6e3M6NDoibmFtZSI7czo0OiJuYW1lIjt9',
-        $serialized_string
+        new NameJob('name'),
+        HaystackBale::first()->job
     );
 });
